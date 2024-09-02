d934e3ae562165b178d304c163613d2ab1bc5ef1
asas6978 <asas6978@naver.com>
2024-09-02 20:41:13 +0900

    Chore - prometheus 설정 추가
    <footer>
    - 관련: #29

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,20 +22,34 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // kotlin
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // mysql
     runtimeOnly("mysql:mysql-connector-java:8.0.33")
+
+    // test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("io.rest-assured:spring-mock-mvc")
     testImplementation("org.assertj:assertj-core:3.25.1")
     testImplementation("io.mockk:mockk:1.13.7")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
     testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
     testImplementation("io.kotest:kotest-assertions-core:5.9.1")
     testImplementation("io.kotest:kotest-framework-api:5.9.1")
+
+    // swagger
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
+
+    // env
     implementation("io.github.cdimascio:dotenv-java:3.0.0")
+
+    // monitoring
+    implementation("io.micrometer:micrometer-registry-prometheus")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 }
 
 kotlin {
