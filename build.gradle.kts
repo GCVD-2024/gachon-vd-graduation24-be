bd62093e34b8bfb1a6a97f56b8d4602b19b13f10
asas6978 <asas6978@naver.com>
2024-10-07 14:22:30 +0900

    Feature - 방명록 조회 API 캐싱 추가
    <footer>
    - 관련: #45

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,14 +22,17 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-data-redis")
 
     // kotlin
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
-    // mysql
+    // database
     runtimeOnly("mysql:mysql-connector-java:8.0.33")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
+    // cache
+    implementation("org.springframework.boot:spring-boot-starter-cache")
 
     // test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
