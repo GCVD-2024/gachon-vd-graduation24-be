98bc9f2dae658e2a86450a853121a3d30b9b7a40
asas6978 <asas6978@naver.com>
2024-10-14 23:08:08 +0900

    Bug - 타임존 명시적으로 설정
    <footer>
    - 관련: #51

--- src/main/kotlin/org/gcvd/server/ServerApplication.kt
+++ src/main/kotlin/org/gcvd/server/ServerApplication.kt
@@ -2,10 +2,13 @@ package org.gcvd.server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.*
 
 @SpringBootApplication
 class ServerApplication
 
 fun main(args: Array<String>) {
-	runApplication<ServerApplication>(*args)
+    runApplication<ServerApplication>(*args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
+    }
 }
